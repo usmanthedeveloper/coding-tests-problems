3ad4a5bbe5a07821d5158b957609204b5a14f8b3
Usman <usmanthedeveloper@gmail.com>
2020-08-18 16:31:56 +0100

    added functions and cleaned up some code

--- GitHubApILangsFinder/github.php
+++ GitHubApILangsFinder/github.php
@@ -1,67 +1,50 @@
 <?php
+//Testing language analysis from GitHUB API
+
 
 class GitHubLangChecker
 {
 
     var $token = "596388b278c5df6583efee31631ee903516cd885";
     var $baseURL = "https://api.github.com";
-
+    var $username = "";
 
     function __construct($username)
     {
 
-
+        $this->$username = $username;
     }
 
+    function createConn($pathURL)
+    {
 
-    function createConn(){
-
-        $ch = curl_init($this->baseURL . );
-        $authorization = "Authorization: Bearer " . $token; // Prepare the authorisation token
+        header('Content-Type: application/json');
+        $ch = curl_init($this->baseURL . $this->token);
+        $authorization = "Authorization: Bearer " . $pathURL; // Prepare the authorisation token
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+        $config['useragent'] = 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0';
+        curl_setopt($ch, CURLOPT_USERAGENT, $config['useragent']);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
 
+    function getRepositories()
+    {
+
+        echo $this->createConn("users/" . $this->username);
 
     }
 
-    function getRepositories(){
+    function getLangs()
+    {
 
-
-
+        //Get repos of user first
+        //https://api.github.com/repos/jbj/avl_tree_set_rs/languages
+        echo $this->createConn("users/" . $this->username);
     }
-
-    function getLangs(){
-
-
-
-
-    }
-
-
-    
-
-
-    
 }
 
-function getGitHubUserData($url)
-{
-    $token = "596388b278c5df6583efee31631ee903516cd885";
-
-    header('Content-Type: application/json');
-
- 
-
-    $config['useragent'] = 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0';
-
-    curl_setopt($ch, CURLOPT_USERAGENT, $config['useragent']);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-}
-
-//echo getGitHubUserData("");
-
-
-echo getGitHubUserData("https://api.github.com/repos/jbj/avl_tree_set_rs/languages");
+$gitHubLangCheck = new GitHubLangChecker($jbj);
+$gitHubLangCheck->getRepositories();
